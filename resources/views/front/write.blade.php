6df71a7891daf42674aca7be804908574abae10d
AlbertWebs <albertmuhatia@gmail.com>
2024-06-06 17:07:18 +0300

    Update

--- resources/views/front/write.blade.php
+++ resources/views/front/write.blade.php
@@ -9,17 +9,17 @@
                 <div class="col-lg-8">
                     <div class="section-title-wrap mb-30">
                         <div class="section-title">
-                            <h2 class="title">Step 1</h2>
+                            <h2 class="title">NOTES FOR AUTHORS</h2>
                         </div>
                         <div class="view-all-btn">
-                            <a href="#next" class="link-btn">Write For Us
+                            {{-- <a href="#next" class="link-btn">Write For Us
                                 <span class="svg-icon">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" fill="none">
                                         <path d="M1.07692 10L0 8.92308L7.38462 1.53846H0.769231V0H10V9.23077H8.46154V2.61538L1.07692 10Z" fill="currentColor" />
                                         <path d="M1.07692 10L0 8.92308L7.38462 1.53846H0.769231V0H10V9.23077H8.46154V2.61538L1.07692 10Z" fill="currentColor" />
                                     </svg>
                                 </span>
-                            </a>
+                            </a> --}}
                         </div>
                         <div class="section-title-line"></div>
                     </div>
@@ -32,85 +32,56 @@
                               <div class="wfu-wrapper">
 
 
-                                <h4>Here are some of our terms for writers</h4>
+                                {{-- <h3></h3> --}}
                                 <div class="wfu-content mt-5" style="justify-content: center;">
                                     <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                        Duis
-                                        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                                        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                                        <strong>CONTENT:</strong> Articles submitted to African Pharmaceutical Review need to be original
+                                        content unpublished in other platforms. They should be devoid of commercial bias
+                                        <strong>[avoiding the mention of specific brand products and vendor companies].</strong>
+                                        Authors can only mention products/processes when this is vital to support the technical
+                                        context of the content.
                                     </p>
                                     <br>
                                     <p>
-                                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque
-                                        corrupti
-                                        quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
-                                        officia
-                                        deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
-                                        Nam
-                                        libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere
-                                        possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis
-                                        aut
-                                        rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum
-                                        hic
-                                        tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus
-                                        asperiores repellat
+                                        <strong>LANGUAGE:</strong> All content must be written in the English Language.<br>
+                                        <strong>WORD COUNT:</strong> Articles should be approximately between 1000 – 1200 words in
+                                        length, excluding references.<br>
+                                        <strong>REFERENCES:</strong> Should be cited throughout the text and ensure all references are listed
+                                        at the end of the article in the references list. [Refer to American Medical Association
+                                        (AMA) Style of referencing]
                                     </p>
                                     <br>
                                     <p>
-                                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque
-                                        corrupti
-                                        quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
-                                        officia
-                                        deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
-                                        Nam
-                                        libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere
-                                        possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis
-                                        aut
-                                        rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum
-                                        hic
-                                        tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus
-                                        asperiores repellat
+                                        <strong>SUBMISSION:</strong> Authors should submit their content (in Microsoft Word format) to the
+                                        email: <a href="mailto:editor@africanpharmaceuticalreview.com">editor@africanpharmaceuticalreview.com</a> . Article submitted should include; title
+                                        of the article, author name, job title, date of submission and institution affiliations if any.<br><br>
+
+                                        <strong>IMAGES/GRAPHS/FIGURES:</strong> All images to illustrate the article should be supplied in
+                                        high resolution jpg files. Tables, graphs and figures should be submitted in their original
+                                        source format i.e. excel, word or otherwise and they should be properly cited in the
+                                        article.<br><br>
+
+                                        <strong>AUTHOR BIOGRAPHY</strong> &amp; PHOTOGRAPH: Authors should supply a brief biography (50
+                                        -85 words) of themselves and a high-resolution print quality photograph.
+                                        <br><br>
+                                        <strong>EDITING:</strong> The publisher reserves the right to edit any article as necessary while striving
+                                        to maintain its original context.<br><br>
+                                        <strong>Extra notes</strong>
+                                        This is not a peer-reviewed publication.
+
+                                        It’s not in our protocol to send a proof back to the author before publication but we
+                                        commit to ensuring your content is shared in its highest form of quality as soon as viably
+                                        possible.
+                                        For further questions, clarifications and enquiries, please contact the editor at
+                                        <a href="mailto:editor@africanpharmaceuticalreview.com">editor@africanpharmaceuticalreview.com</a>
                                     </p>
-                                    <br>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                        Duis
-                                        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                                        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                                    </p>
-                                    <br>
-                                    <p>
-                                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque
-                                        corrupti
-                                        quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
-                                        officia
-                                        deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
-                                        Nam
-                                        libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere
-                                        possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis
-                                        aut
-                                        rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum
-                                        hic
-                                        tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus
-                                        asperiores repellat
-                                    </p>
-                                    <br>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                        Duis
-                                        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                                        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                                    </p>
+
                                 </div>
 
 
 
                                 <!-- <div class="reg-header-right col-lg-6 col-md-12"></div> -->
-                                <p>
+                                {{-- <p>
                                     <div class="pull-right" id="next">
                                         <div class="view-all-btn">
                                             <a href="{{url('/')}}/write-for-us/step-2" class="link-btn">Proceed <i class="fa fa-arrow-right"></i>
@@ -118,7 +89,7 @@
                                             </a>
                                         </div>
                                     </div>
-                                </p>
+                                </p> --}}
 
 
 
