f49a8c1d779164458c65a58068e2fef07685deca
AlbertWebs <albertmuhatia@gmail.com>
2024-06-12 16:59:59 +0300

    upload ck editor

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -175,11 +175,11 @@ class HomeController extends Controller
                 $fileName = pathinfo($originName, PATHINFO_FILENAME);
                 $extension = $request->file('upload')->getClientOriginalExtension();
                 $fileName = $fileName . '_' . time() . '.' . $extension;
-                $path = base_path('public/uploads/ck');
+                $path = base_path('uploads/ck');
                 $fileName = $request->file('upload')->move($path, $fileName)->getFilename();
 
                 $CKEditorFuncNum = $request->input('CKEditorFuncNum');
-                $url = asset('public/uploads/ck/' . $fileName);
+                $url = asset('uploads/ck/' . $fileName);
                 $msg = 'Image uploaded successfully';
                 $response = "<script>window.parent.CKEDITOR.tools.callFunction($CKEditorFuncNum, '$url', '$msg')</script>";
 
