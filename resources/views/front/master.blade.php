9f29ab01a5bbeaf514fe2a171156c7cd06b3c772
AlbertWebs <albertmuhatia@gmail.com>
2024-07-05 17:30:40 +0300

    Update

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -15,15 +15,16 @@
 
         {{--  --}}
         <title>African Pharmaceutical Review - Keep up to date with the Latest News, Articles and Insights on the drug manufacturing, trade, technology and pharma supply chains in Africa.</title>
-        <meta name="description" content="Get the latest news, interviews insights and developments of the African Pharmaceutical Industry. African Pharmaceutical Review keeps you up to date." />
-		<meta name="robots" content="max-image-preview:large" />
+        <meta name="description" content="Get the latest news, interviews insights and developments of the African Pharmaceutical Industry." />
+		<meta name="keywords" content="African Pharmaceutical Industry news, African Pharma updates, Pharmaceutical industry in Africa, Latest African pharmaceutical developments, African pharma insights, Pharma news Africa, African pharmaceutical interviews, Pharma industry news Africa, Developments in African pharmaceutical sector, African pharma industry trends, African pharmaceutical market updates, Africa pharmaceutical research news, African pharma sector insights, Pharmaceutical innovations in Africa, News on African pharmaceutical companies">
+        <meta name="robots" content="index, follow">
 		<link rel="canonical" href="{{url('/')}}" />
 
 		<meta property="og:locale" content="en_US" />
 		<meta property="og:site_name" content="African Pharmaceutical Review" />
 		<meta property="og:type" content="website" />
 		<meta property="og:title" content="African Pharmaceutical Review - Keep up to date with the Latest News, Articles and Insights on the drug manufacturing, trade, technology and pharma supply chains in Africa." />
-		<meta property="og:description" content="Get the latest news, interviews insights and developments of the African Pharmaceutical Industry. African Pharmaceutical Review keeps you up to date." />
+		<meta property="og:description" content="Get the latest news, interviews insights and developments of the African Pharmaceutical Industry." />
 		<meta property="og:url" content="{{url('/')}}" />
 		<meta property="og:image" content="{{url('/')}}/uploads/logo/log.png" />
 		<meta property="og:image:secure_url" content="{{url('/')}}/uploads/logo/log.png" />
@@ -32,8 +33,8 @@
 
 		<meta name="twitter:card" content="summary_large_image" />
 		<meta name="twitter:site" content="@af_pharmareview" />
-		<meta name="twitter:title" content="African Pharmaceutical Review - Home" />
-		<meta name="twitter:description" content="Get the latest news, interviews insights and developments of the African Pharmaceutical Industry. African Pharmaceutical Review keeps you up to date." />
+		<meta name="twitter:title" content="African Pharmaceutical Review - Keep up to date with the Latest News, Articles and Insights on the drug manufacturing, trade, technology and pharma supply chains in Africa." />
+		<meta name="twitter:description" content="Get the latest news, interviews insights and developments of the African Pharmaceutical Industry." />
 		<meta name="twitter:creator" content="@af_pharmareview" />
 		<meta name="twitter:image" content="{{url('/')}}/uploads/logo/log.png" />
 
