f609d190dcb1f46e1eb643bcaa44d15bd9410693
AlbertWebs <albertmuhatia@gmail.com>
2024-08-09 09:25:58 +0300

    update

--- resources/views/front/footer-ad.blade.php
+++ resources/views/front/footer-ad.blade.php
@@ -7,7 +7,7 @@
             <div class="pharverse-fixed-footer pharverse-sticky advertisement-banner" id="footer-ad">
                 <span class="pharverse-close-button pharverse-close-button-super" id="pharverse-close-button"  title="close"> × </span>
                 <div id="placement_fixed_footer">
-                    <a id="pharverse-ad-whitepapers-btn-Footer" href="{{$footerAd->url}}" target="_blank" rel="nofollow" class="placement_fixed_footer-img">
+                    <a id="pharverse-ad-whitepapers-btn-Footer" href="{{$footerAd->url}}" target="new"  class="placement_fixed_footer-img">
                         <img src="{{$footerAd->image}}" alt="Footer Ad" title="Footer Ad" border="0" >
                     </a>
                 </div>
