6aa2ff8c9027ab9354806904526528b2e935f285
AlbertWebs <albertmuhatia@gmail.com>
2024-11-18 10:33:58 +0300

    simple

--- config/reading-time.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'speed' => [
-        'slow' => env('READING_TIME_SPEED_SLOW', 100),
-        'average' => env('READING_TIME_SPEED_AVERAGE', 500),
-        'fast' => env('READING_TIME_SPEED_AVERAGE', 300),
-    ],
-];
--- resources/views/import.blade.php
+++
@@ -1,5 +0,0 @@
-<form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <input type="file" name="file" accept=".csv">
-    <button type="submit">Import CSV</button>
-</form>
