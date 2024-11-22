a3a6af73f738ac33e790df206a768729e92378b8
AlbertWebs <albertmuhatia@gmail.com>
2024-11-22 11:49:40 +0300

    update

--- resources/views/front/master-noads.blade.php
+++ resources/views/front/master-noads.blade.php
@@ -5,7 +5,10 @@
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="shortcut icon" type="image/x-icon" href="{{asset('theme/assetsimg/favicon.png')}}">
+		<link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon/apple-touch-icon.png')}}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon/favicon-32x32.png')}}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon/favicon-16x16.png')}}">
+        <link rel="manifest" href="{{asset('favicon/site.webmanifest')}}">
         <!-- Place favicon.ico in the root directory -->
 
         <title>African Pharmaceutical Review - Keep up to date with the Latest News, Articles and Insights on the drug manufacturing, trade, technology and pharma supply chains in Africa.</title>
